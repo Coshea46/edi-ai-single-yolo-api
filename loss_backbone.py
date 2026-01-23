d29475ff7505a04727ecce655d3820ea99bfec99
Conor O'Shea <s2683460@ed.ac.uk>
2026-01-23 00:54:59 +0000

    fixed loss_backbone constructor

--- loss_backbone.py
+++ loss_backbone.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 
 class loss_backbone(nn.Module):
-    def __init__(self, model, train_loader, device):
-        self.model = model
-        self.train_loader = train_loader
-        self.device = device
-        self.mse = nn.MSELoss(reduction='none') # We use none to keep it as a vector
+    def __init__(self, confidence_weight, coord_weight, loss_function):
+        super().__init__()
+
+        self.confidence_weight = confidence_weight
+        self.coord_weight = coord_weight
 
 
     def forward(self,predictions, targets, loss_function_kaggle):
-        return loss_function_kaggle(predictions, targets)  # function body of loss_function_kaggleshould be implemented in kaggle
+        return loss_function_kaggle(predictions, targets, self.confidence_weight, self.coord_weight)  # function body of loss_function_kaggleshould be implemented in kaggle
     
